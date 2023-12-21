f696adab091ff339dac7bbe8f55e6bb2d837bf72
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2023-12-22 00:36:24 +0100

    Fix S1008
    should use 'return len(msg.R.ID) == 20' instead of 'if len(msg.R.ID) != 20 { return false }; return true'

--- dht/mainline/protocol.go
+++ dht/mainline/protocol.go
@@ -430,14 +430,8 @@ func validatePingORannouncePeerResponseMessage(msg *Message) bool {
 }
 
 func validateFindNodeResponseMessage(msg *Message) bool {
-	//lint:ignore S1008 to be done later
-	if len(msg.R.ID) != 20 {
-		return false
-	}
-
 	// TODO: check nodes field
-
-	return true
+	return len(msg.R.ID) == 20
 }
 
 func validateGetPeersResponseMessage(msg *Message) bool {
