f8bc717e472029fa0e0bbd31b85c3978ec68cef7
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-02-29 00:15:04 +0100

    test(persistence): additional testing for iso8601

--- persistence/iso8601_test.go
+++ persistence/iso8601_test.go
@@ -1,6 +1,9 @@
 package persistence
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 var validDates = []struct {
 	date        string
@@ -41,6 +44,55 @@ func TestParseISO8601(t *testing.T) {
 			t.Errorf("Granularity of the date #%d is wrong! Got %d (expected %d)",
 				i+1, gr, date.granularity)
 			continue
+		}
+	}
+}
+
+func TestDaysOfMonth(t *testing.T) {
+	tests := []struct {
+		month time.Month
+		year  int
+		want  int
+	}{
+		{time.January, 2022, 31},
+		{time.February, 2022, 28},
+		{time.February, 2024, 29},
+		{time.March, 2022, 31},
+		{time.April, 2022, 30},
+		{time.May, 2022, 31},
+		{time.June, 2022, 30},
+		{time.July, 2022, 31},
+		{time.August, 2022, 31},
+		{time.September, 2022, 30},
+		{time.October, 2022, 31},
+		{time.November, 2022, 30},
+		{time.December, 2022, 31},
+	}
+
+	for _, tt := range tests {
+		got := daysOfMonth(tt.month, tt.year)
+		if got != tt.want {
+			t.Errorf("daysOfMonth(%v, %v) = %v, want %v", tt.month, tt.year, got, tt.want)
+		}
+	}
+}
+
+func TestIsLeap(t *testing.T) {
+	tests := []struct {
+		year int
+		want bool
+	}{
+		{2000, true},
+		{2004, true},
+		{2100, false},
+		{2200, false},
+		{2400, true},
+	}
+
+	for _, tt := range tests {
+		got := isLeap(tt.year)
+		if got != tt.want {
+			t.Errorf("isLeap(%v) = %v, want %v", tt.year, got, tt.want)
 		}
 	}
 }
